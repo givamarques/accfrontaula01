bd43764b1ca8bc94b418ee584d1fcf36682571cf
givamarques <49195218+givamarques@users.noreply.github.com>
2021-07-13 19:16:33 -0300

    02

--- class-component/src/index.js
+++ class-component/src/index.js
@@ -5,9 +5,24 @@ import './index.css';
 //class app - esse componente vai renderizar um h1 dizendo "hello world"
 
 class App extends React.Component{
-  render(){
-    return<h1>Hello World {this.props.name}</h1>
+  state = {
+    texto: “ Turma React”
   }
+  handleClick(){
+    alert(”Alerta ativado!“)
+  }
+  handleTextCharge(e){
+    this.setState({
+      texto: e.target.value
+    })
+  }
+
+  render(){
+    return(<div><h1>Hello World {this.props.name}</h1>
+    <h2>{this.state.texto}</h2>
+    <input type=”text” onChange={this.handleTextCharge.bind(this)}></input>
+    <button onClick={this.handleClick.bind(this)}>Exibir Alerta</button>
+  )}
 }
 
 ReactDOM.render(
